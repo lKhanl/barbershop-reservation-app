af67e15fa5aea7454f49c16732a9a44eb2c6d03b
lKhanl <oguzhanercelik@eskisehir.edu.tr>
2021-05-20 05:01:53 +0300

    updated

--- src/main/java/edu/eskisehir/AdminController.java
+++ src/main/java/edu/eskisehir/AdminController.java
@@ -1,11 +1,16 @@
 package edu.eskisehir;
 
 import javafx.event.ActionEvent;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.event.EventType;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class AdminController {
     public Pane mainPane;
@@ -14,8 +19,36 @@ public class AdminController {
     public TextField txtUserName;
     public PasswordField txtPassword;
     public Button btnLogin;
+    public AnchorPane ap;
 
-    public void login(ActionEvent event) {
-        System.out.println("loginnnnnnnnnnnnnnnn");
+    public void login(ActionEvent event) throws IOException {
+        DataBaseOperations db = new DataBaseOperations();
+
+        txtUserName.setText("admin");
+        txtPassword.setText("admin");
+
+        String adminUserName = txtUserName.getText();
+        String adminPass = txtPassword.getText();
+
+
+
+        System.out.println(adminUserName + " " + adminPass);
+
+        if (db.checkAdmin(adminUserName, adminPass)) {
+
+            Stage adminStg = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            adminStg.close();
+            ((Stage)adminStg.getOwner()).close();
+
+            Stage stage = Main.openNewStage("AdminScreen", AdminScreenController.class);
+            stage.setTitle("Admin Screen");
+            stage.show();
+
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Wrong username or password!");
+            alert.show();
+        }
     }
 }
