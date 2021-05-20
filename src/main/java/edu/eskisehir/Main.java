458d3f7bea5b522353f73c25c175966ddba0e118
lKhanl <oguzhanercelik@eskisehir.edu.tr>
2021-05-20 03:02:00 +0300

    checkAdmin added

--- src/main/java/edu/eskisehir/Main.java
+++ src/main/java/edu/eskisehir/Main.java
@@ -4,9 +4,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
