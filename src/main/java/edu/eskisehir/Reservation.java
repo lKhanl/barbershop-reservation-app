33266d13754074075d1adbf4097f138b30cc5192
lKhanl <oguzhanercelik@eskisehir.edu.tr>
2021-05-30 02:03:13 +0300

    Reservation is created

---
+++ src/main/java/edu/eskisehir/Reservation.java
@@ -0,0 +1,68 @@
+package edu.eskisehir;
+
+import java.sql.Date;
+import java.sql.Time;
+import java.util.List;
+
+public class Reservation {
+    private long id;
+    private Date date;
+    private Time time;
+    private int cost;
+    private Barber barber;
+    private List<Operation> ops;
+    private int isDone;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Time getTime() {
+        return time;
+    }
+
+    public void setTime(Time time) {
+        this.time = time;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    public Barber getBarber() {
+        return barber;
+    }
+
+    public void setBarber(Barber barber) {
+        this.barber = barber;
+    }
+
+    public List<Operation> getOps() {
+        return ops;
+    }
+
+    public int getIsDone() {
+        return isDone;
+    }
+
+    public void setIsDone(int isDone) {
+        this.isDone = isDone;
+    }
+
+}
